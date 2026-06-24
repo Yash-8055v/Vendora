769b13a490ebe7c6e9d5a2d5e2c78f2aa1c984ea
Yash Karande <yashkarande4240@gmail.com>
2026-06-24 23:49:32 +0530

    otp generate utility added

--- backend/src/utils/generateOtp.js
+++ backend/src/utils/generateOtp.js
@@ -1,10 +1,10 @@
 import bcrypt from 'bcrypt';
 
 export const generateOTP = async () => {
-  const otp =  (Math.floor(100000 + Math.random() * 900000)).toString();
+  const otp = (Math.floor(100000 + Math.random() * 900000)).toString();
 
   const hashedOtp = await bcrypt.hash(otp, 10);
 
   return {otp, hashedOtp};
-  
 }
+
