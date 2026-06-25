13196984454018ec0f3d1056c9a1783db49dc727
Yash Karande <yashkarande4240@gmail.com>
2026-06-25 23:20:05 +0530

    reset password validations added

--- backend/src/modules/auth/auth.validation.js
+++ backend/src/modules/auth/auth.validation.js
@@ -1,46 +1,43 @@
 import { email, string, z } from "zod";
 
-export const registerUserValidation = z.object({
-  name: z
-    .string({ required_error: "Name is required" })
-    .trim()
-    .min(2, { message: "Name must be at least 2 characters long" })
-    .max(60, { message: "Name cannot exceed 60 characters" }),
+export const registerUserValidation = z
+  .object({
+    name: z
+      .string({ required_error: "Name is required" })
+      .trim()
+      .min(2, { message: "Name must be at least 2 characters long" })
+      .max(60, { message: "Name cannot exceed 60 characters" }),
 
-  email: z
-    .string({ required_error: "Email is required" })
-    .trim()
-    .toLowerCase()
-    .min(1, { message: "Email address cannot be empty" })
-    .email({ message: "Please enter a valid email address" }),
+    email: z
+      .string({ required_error: "Email is required" })
+      .trim()
+      .toLowerCase()
+      .min(1, { message: "Email address cannot be empty" })
+      .email({ message: "Please enter a valid email address" }),
 
-  password: z
-  .string()
-  .min(8, { message: "Password must be at least 8 characters long" })
-  .refine((val) => /[A-Z]/.test(val), {
-    message: "Password must contain at least one uppercase letter (A-Z)",
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" })
+      .refine((val) => /[A-Z]/.test(val), {
+        message: "Password must contain at least one uppercase letter (A-Z)",
+      })
+      .refine((val) => /[a-z]/.test(val), {
+        message: "Password must contain at least one lowercase letter (a-z)",
+      })
+      .refine((val) => /[0-9]/.test(val), {
+        message: "Password must contain at least one number (0-9)",
+      })
+      .refine((val) => /[^A-Za-z0-9]/.test(val), {
+        message: "Password must contain at least one special character",
+      }),
+    conformPassword: z.string(),
   })
-  .refine((val) => /[a-z]/.test(val), {
-    message: "Password must contain at least one lowercase letter (a-z)",
-  })
-  .refine((val) => /[0-9]/.test(val), {
-    message: "Password must contain at least one number (0-9)",
-  })
-  .refine((val) => /[^A-Za-z0-9]/.test(val), {
-    message: "Password must contain at least one special character",
-  }),
-  conformPassword: z.string()
-    
-  
-}).refine((data) => data.password === data.conformPassword, {
-  message: "Passwords don't match",
-  path: ["conformPassword"],
-});
-
-
+  .refine((data) => data.password === data.conformPassword, {
+    message: "Passwords don't match",
+    path: ["conformPassword"],
+  });
 
 export const loginUserValidation = z.object({
-  
   email: z
     .string({ required_error: "Email is required" })
     .trim()
@@ -48,11 +45,54 @@ export const loginUserValidation = z.object({
     .min(1, { message: "Email address cannot be empty" })
     .email({ message: "Please enter a valid email address" }),
 
-  password: z
-  .string()
-  .nonempty({message: "Password is required"})
-  
-})
-
+  password: z.string().nonempty({ message: "Password is required" }),
+});
 
 export const tokenValidation = z.string().jwt().nonempty(" token is required");
+
+export const otpValidation = z.object({
+  otp: z
+    .string()
+    .length(6, { message: "OTP must be exactly 6 digits." })
+    .regex(/^\d+$/, { message: "OTP must contain only numbers." }),
+});
+
+export const forgotPasswordValidation = z.object({
+  email: z.string().email("Invalid email").trim().toLowerCase(),
+});
+
+export const verifyResetPasswordOtpValidation = z.object({
+  email: z.string().email("Invalid email").trim().toLowerCase(),
+  otp: z
+    .string()
+    .length(6, { message: "OTP must be exactly 6 digits." })
+    .regex(/^\d+$/, { message: "OTP must contain only numbers." }),
+});
+
+export const resetPasswordValidation = z.object({
+  email: z
+    .string({ required_error: "Email is required" })
+    .trim()
+    .toLowerCase()
+    .min(1, { message: "Email address cannot be empty" })
+    .email({ message: "Please enter a valid email address" }),
+  otp: z
+    .string()
+    .length(6, { message: "OTP must be exactly 6 digits." })
+    .regex(/^\d+$/, { message: "OTP must contain only numbers." }),
+  newPassword: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .refine((val) => /[A-Z]/.test(val), {
+      message: "Password must contain at least one uppercase letter (A-Z)",
+    })
+    .refine((val) => /[a-z]/.test(val), {
+      message: "Password must contain at least one lowercase letter (a-z)",
+    })
+    .refine((val) => /[0-9]/.test(val), {
+      message: "Password must contain at least one number (0-9)",
+    })
+    .refine((val) => /[^A-Za-z0-9]/.test(val), {
+      message: "Password must contain at least one special character",
+    }),
+});
