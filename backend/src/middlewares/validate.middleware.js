570d4912f98ee6ea8f9fd8112b41c29533feba63
Yash Karande <yashkarande4240@gmail.com>
2026-06-25 23:25:42 +0530

    reset password validation middleware added

--- backend/src/middlewares/validate.middleware.js
+++ backend/src/middlewares/validate.middleware.js
@@ -35,3 +35,32 @@ export const validateToken = (schema) => async (req, res, next) => {
   return next();
 }
 
+export const validateOTP = (schema) => async (req, res, next) => {
+  const otp = req.body;
+  
+  
+  
+  const result = await schema.safeParseAsync(otp);
+
+  if(!result.success) {
+    return res.status(400).json({
+      success: false,
+      message: result.error.format()
+    })
+  }
+
+  
+
+  return next();
+
+}
+
+
+export const resetPasswordController = async (req, res) => {
+  try {
+    const {email, otp, newPassword} = req.body;
+    await resetPasswordService(email, otp, newPassword);
+  } catch (error) {
+    
+  }
+}
