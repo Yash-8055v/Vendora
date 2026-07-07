6ab639221fe3896a57227b6b8b8f920b47670e21
Yash Karande <yashkarande4240@gmail.com>
2026-07-07 23:04:42 +0530

    new routes added

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,13 +12,19 @@ import cartRouter from './modules/cart/cart.routes.js';
 import addressRouter from './modules/address/address.routes.js';
 import orderRouter from './modules/order/order.routes.js';
 import vendorOrderRouter from './modules/order/vendor-order.routes.js';
-
-
+import upload from "./modules/media/middleware/upload.js";
+import { uploadImage } from "./modules/media/services/upload.service.js";
+import heroRouter from './modules/hero/routes/hero.routes.js';
+import homeRouter from './modules/home/routes/home.routes.js';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.VITE_API_URL || "http://localhost:5173",
+  credentials: true,
+  
+}));
 app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
@@ -31,6 +37,24 @@ app.use('/api/v1/cart', cartRouter);
 app.use('/api/v1/addresses', addressRouter);
 app.use('/api/v1/orders', orderRouter);
 app.use('/api/v1/vendor/orders', vendorOrderRouter);
+app.use('/api/v1/hero-slides', heroRouter);
+app.use('/api/v1/home', homeRouter);
+
+app.post(
+  "/upload",
+  upload.single("image"),
+  async (req, res) => {
+    const result = await uploadImage(req.file, "test");
+
+    return res.json({
+      success: true,
+      image: {
+        url: result.secure_url,
+        publicId: result.public_id,
+      },
+    });
+  }
+);
 
 app.get("/health", (req, res) => {
   res.status(200).json({
