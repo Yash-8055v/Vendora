136b1f675067eb8e932d29fa5a1c5b83b11142d8
Yash Karande <yashkarande4240@gmail.com>
2026-07-07 23:15:19 +0530

    home page routes

---
+++ backend/src/modules/home/routes/home.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getHomeController } from '../controllers/home.controller.js';
+
+
+const router = express.Router();
+
+
+router.get("/", getHomeController);
+
+
+
+export default router;
